20615ab83a68c6235d73ce83cd1d78c7366cf279
Martin Ingram <martin.ingram@gmail.com>
2021-12-21 12:53:06 +0100

    Update fit script

--- fit_all.sh
+++ fit_all.sh
@@ -3,8 +3,7 @@ target_dir="/media/martin/big_extra_space/pymc_vs_stan/rerun_20_12_2021/fits"
 
 random_seed=2
 
-# for start_year in 2020 2019 2015 2010 2000 1990 1980 1968; do
-for start_year in 1980 1968; do
+for start_year in 2020 2019 2015 2010 2000 1990 1980 1968; do
     echo "Fitting $start_year"
     echo "PyMC JAX GPU parallel" && python fit_pymc_jax.py $start_year gpu parallel "$target_dir" $random_seed
     echo "PyMC JAX GPU vectorized" && python fit_pymc_jax.py $start_year gpu vectorized "$target_dir" $random_seed
