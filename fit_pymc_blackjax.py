b25d51a8ad60d2091d5b2d2a4015dd24b027d73c
Martin Ingram <martin.ingram@gmail.com>
2022-10-23 12:36:59 +0200

    Add blackjax

--- fit_pymc_blackjax.py
+++ fit_pymc_blackjax.py
@@ -9,6 +9,7 @@ start_year = int(sys.argv[1])
 platform = sys.argv[2]
 base_dir = sys.argv[3]
 seed = int(sys.argv[4])
+chain_method = sys.argv[5]
 
 assert platform in ["cpu", "gpu"]
 
@@ -16,7 +17,7 @@ if platform == "cpu":
     # Disable GPU
     os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
-target_dir = f"{base_dir}/pymc_blackjax_{platform}"
+target_dir = f"{base_dir}/pymc_blackjax_{platform}_{chain_method}"
 
 os.makedirs(target_dir, exist_ok=True)
 
@@ -25,7 +26,10 @@ model = get_pymc_model(start_year=start_year)
 start_time = time()
 
 with model:
-    hierarchical_trace = pymc.sampling_jax.sample_blackjax_nuts(random_seed=seed)
+    # No progress bar?
+    hierarchical_trace = pymc.sampling_jax.sample_blackjax_nuts(
+        random_seed=seed, chain_method=chain_method,
+        idata_kwargs={'log_likelihood': False})
 
 runtime = time() - start_time
 
